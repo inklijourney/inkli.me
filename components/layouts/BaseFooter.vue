77d3b42be3df5bfdb83ee3c22a3f0875c883644d
inkli <lihj.1015@gmail.com>
2023-09-11 16:43:49 +0800

    添加页脚

---
+++ components/layouts/BaseFooter.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <p class="text-zinc-400 text-sm mt-10">
+    <NuxtLink to="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">
+      CC BY-NC-SA 4.0 2023-PRESENT © inkli
+    </NuxtLink>
+  </p>
+</template>
+
+<style scoped>
+
+</style>
